a8cee2c646c560a55f74b99184271b4973fccc1e
Larin Sergey <direct808@yandex.ru>
2026-03-11 16:18:27 +0300

    mw

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,15 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+
+const darkTheme = createTheme({ palette: { mode: 'dark' } })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
