0f21a24d9e63dfb6bd120e510535dd661c412e65
Larin Sergey <direct808@yandex.ru>
2026-03-11 20:57:47 +0300

    mw

--- src/components/AssetsWidget.tsx
+++ src/components/AssetsWidget.tsx
@@ -1,8 +1,15 @@
+import { useState } from 'react'
 import { useAssetStore } from '../stores'
 import {
+  Button,
   Card,
   CardContent,
   CardHeader,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Stack,
   Table,
   TableBody,
   TableCell,
@@ -11,39 +18,113 @@ import {
   TextField,
 } from '@mui/material'
 
-export function AssetsWidget() {
-  const { assets, updatePrice } = useAssetStore()
+function AddAssetDialog({
+  open,
+  onClose,
+}: {
+  open: boolean
+  onClose: () => void
+}) {
+  const { add } = useAssetStore()
+  const [code, setCode] = useState('')
+  const [price, setPrice] = useState('')
+
+  const handleSave = () => {
+    add({ code, price: +price })
+    setCode('')
+    setPrice('')
+    onClose()
+  }
+
+  const handleCancel = () => {
+    setCode('')
+    setPrice('')
+    onClose()
+  }
 
   return (
-    <Card>
-      <CardHeader title="Assets" />
-      <CardContent>
-        <Table size="small">
-          <TableHead>
-            <TableRow>
-              <TableCell>Code</TableCell>
-              <TableCell>Price</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {assets.map((asset) => (
-              <TableRow key={asset.code}>
-                <TableCell>{asset.code}</TableCell>
-                <TableCell>
-                  <TextField
-                    size="small"
-                    type="number"
-                    value={asset.price}
-                    onChange={(event) =>
-                      updatePrice(asset.code, +event.target.value)
-                    }
-                  />
-                </TableCell>
+    <Dialog open={open} onClose={handleCancel}>
+      <DialogTitle>Add Asset</DialogTitle>
+      <DialogContent>
+        <Stack spacing={2} sx={{ pt: 1 }}>
+          <TextField
+            label="Code"
+            size="small"
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+          />
+          <TextField
+            label="Price"
+            size="small"
+            type="number"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </Stack>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleCancel}>Отмена</Button>
+        <Button
+          variant="contained"
+          onClick={handleSave}
+          disabled={!code || !price}
+        >
+          Сохранить
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export function AssetsWidget() {
+  const { assets, updatePrice } = useAssetStore()
+  const [dialogOpen, setDialogOpen] = useState(false)
+
+  return (
+    <>
+      <Card>
+        <CardHeader
+          title="Assets"
+          action={
+            <Button
+              sx={{ pt: 1 }}
+              size="small"
+              onClick={() => setDialogOpen(true)}
+            >
+              + Add
+            </Button>
+          }
+        />
+        <CardContent>
+          <Table size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell>Code</TableCell>
+                <TableCell>Price</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </CardContent>
-    </Card>
+            </TableHead>
+            <TableBody>
+              {assets.map((asset) => (
+                <TableRow key={asset.code}>
+                  <TableCell>{asset.code}</TableCell>
+                  <TableCell>
+                    <TextField
+                      size="small"
+                      type="number"
+                      value={asset.price}
+                      onChange={(event) =>
+                        updatePrice(asset.code, +event.target.value)
+                      }
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+
+      <AddAssetDialog open={dialogOpen} onClose={() => setDialogOpen(false)} />
+    </>
   )
 }
